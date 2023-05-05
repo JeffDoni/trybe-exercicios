840e08bafd54a09e2169676b7a056cc714c211f4
Jefferson Donizete do Nascimento <jdn1937@hotmail.com>
2023-05-05 09:14:45 -0300

    exercicios sessao 4

---
+++ back-end/secao-04-Introducao-ao-desenvolvimento-Web-com-Node.js/dia-05-Nodejs-Express-e-MySQL/trybecash-api/src/routes/peopleRoutes.js
@@ -0,0 +1,12 @@
+// src/routes/peopleRoutes.js
+
+const express = require('express');
+
+const router = express.Router();
+
+router.post('/', (req, res) => {
+  const person = req.body;
+  res.status(201).json(person);
+});
+
+module.exports = router;
